723ba121ab7a56d2659528b60fb3f9bba19fa399
Hannu Hyvärinen <hajatahy@student.jyu.fi>
2018-09-09 11:33:03 +0300

    started adding unit tests

---
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+// this is how to connect enzyme
+configure({adapter: new Adapter()});
+
+// this describe and it is from jest, jest is automatically available due to create-react-app setup
+// first arguments describes what to test, doesn't need to be jsx element. This is what you see in the console output
+describe('<NavigationItems />', () => {
+    let wrapper;
+    //this is run before each 'it' function (test)
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
+        // shallow renders the given element, its child components are only placeholders, so that the element is not fully rendered. it's used to test isolated components.
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        // wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated: true});
+
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render logout navigation item if user is authenticated', () => {
+        // props have to set again! beforeEach function is alway run!
+        wrapper.setProps({isAuthenticated: true});
+
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
+});
