b46ae4aaed6033157aeaf1b5808010855661615c
Hannu Hyvärinen <hajatahy@student.jyu.fi>
2018-09-09 11:56:28 +0300

    added reducer test example

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -4,15 +4,17 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { BurgerBuilder } from './BurgerBuilder';
-import BuildControls from '../../components/Burger/BuildControls/BuildControl';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 // this is how to connect enzyme
 configure({adapter: new Adapter()});
 
 describe('<BurgerBuilder />', () => {
     let wrapper;
+
+    // onInitIngredients is used in component did mount so you have to create it for the wrapper also!
     beforeEach(() => {
-        wrapper = shallow(<BurgerBuilder />);
+        wrapper = shallow(<BurgerBuilder onInitIngredients={() => {}}/>);
     });
 
     it('should render <BuildControls /> when receiving ingredients', () => {
